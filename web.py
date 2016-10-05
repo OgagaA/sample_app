c992614fb406525263f782936e532e3cb7c1b90a
Ogaga <ogaga.akoroda@gmail.com>
2016-10-05 16:47:14 -0400

    about me

--- web.py
+++ web.py
@@ -6,6 +6,9 @@ def index():
     #return "Hey world!"
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 #app.run()
 app.run(debug=True)
 # This allows us to see changes as we code rather than have to shut down.
